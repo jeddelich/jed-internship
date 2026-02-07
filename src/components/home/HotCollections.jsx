7b44568e69e41dc080959746b8fda7635e15d797
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-07 17:43:46 -0500

    carousel speed halved and responsiveness added

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -10,9 +10,29 @@ const HotCollections = () => {
 
   const carouselSettings = {
     infinite: true,
-    speed: 500,
+    speed: 250,
     slidesToShow: 4,
     slidesToScroll: 1,
+        responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 570,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   }
 
   const [hotCollectionsData, setHotCollectionsData] = useState()
