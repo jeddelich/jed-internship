afea084e3d9c376dbe29e821b671a5b694116b64
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-08 16:20:38 -0500

    data rendered dynamically

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -16,7 +16,7 @@ const NewItems = () => {
   }
 
   useEffect(() => {
-    requestNewItems()
+    requestNewItems().then(console.log(newItemsData))
   }, []);
   
   return (
@@ -29,21 +29,21 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {newItemsData?.map((_, index) => (
+          {newItemsData?.map((item, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
-                    to="/author"
+                    to={`/author/${item.authorId}`}
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
                     title="Creator: Monica Lucas"
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={item.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                <div className="de_countdown">{item.expiryDate}</div>
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -66,7 +66,7 @@ const NewItems = () => {
 
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={item.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -74,12 +74,12 @@ const NewItems = () => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
