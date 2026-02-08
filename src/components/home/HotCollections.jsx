491dc9ca4ffa361cbdac12b3a3dd9b4398a478e0
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-08 01:01:15 -0500

    skeleton loading state added and responsive

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -35,7 +35,7 @@ const HotCollections = () => {
     ]
   }
 
-  const [hotCollectionsData, setHotCollectionsData] = useState()
+  const [hotCollectionsData, setHotCollectionsData] = useState(null)
 
   async function requestHotCollections() {
     const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
@@ -43,7 +43,7 @@ const HotCollections = () => {
   }
 
   useEffect(() => {
-    requestHotCollections()
+      requestHotCollections()
   }, [])
   
   return (
@@ -56,11 +56,36 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+          { !hotCollectionsData ? ( 
+            <Slider {...carouselSettings}>{new Array(6).fill("").map((collection) => (
+            <div key={collection.id}>
+              <div className="nft_coll mx-1">
+                <div className="nft_wrap" >
+                  <Link to="/item-details/">
+                    <div className="skeleton__img"></div>
+                  </Link>
+                </div>
+                <div className="nft_coll_pp">
+                  <Link to="/author">
+                    <div className="skeleton__profile"></div>
+                  </Link>
+                  <i className="fa fa-check"></i>
+                </div>
+                <div className="nft_coll_info skeleton__description--wrapper">
+                  <Link to="/explore">
+                    <div className="skeleton__title"></div>
+                  </Link>
+                  <span className="skeleton__code"></span>
+                </div>
+              </div>
+            </div>
+          ))}</Slider>
+        ) : (
           <Slider {...carouselSettings}>{hotCollectionsData?.map((collection) => (
             <div key={collection.id}>
               <div className="nft_coll mx-1">
                 <div className="nft_wrap" >
-                  <Link to="/item-details">
+                  <Link to="/item-details/">
                     <img src={collection.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
@@ -78,7 +103,8 @@ const HotCollections = () => {
                 </div>
               </div>
             </div>
-          ))}</Slider>
+          ))}</Slider>)
+          }
         </div>
       </div>
     </section>
