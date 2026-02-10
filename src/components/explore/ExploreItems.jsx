a9e63eb544a98963a2cd4ff920dc864a9303be66
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-10 00:58:01 -0500

    ItemCards component created & applied in ExploreItems & NewItems

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -4,6 +4,7 @@ import nftImage from "../../images/nftImage.jpg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Countdown from "../UI/Countdown";
+import ItemCards from "../UI/ItemCards";
 
 const ExploreItems = () => {
   const [exploreData, setExploreData] = useState(null);
@@ -29,59 +30,11 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreData?.map((item, index) => (
-        <div
-          key={item.id}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to={`/author/${item.authorId}`}
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={item.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to={`/item-details/${item.nftId}`}>
-                <img src={item.nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>{item.title}</h4>
-              </Link>
-              <div className="nft__item_price">{item.price} ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{item.likes}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+      {
+        exploreData?.map((item) =>(
+          <ItemCards key={item.id} item={item} wrapperClass="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 w-full" wrapperStyle={{ display: "block", backgroundSize: "cover" }}/>
+        ))
+      }
       <div className="col-md-12 text-center">
         <Link to="" id="loadmore" className="btn-main lead">
           Load more
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import Slider from "react-slick";
 import { carouselSettings } from "../UI/carouselSettings";
-import Countdown from "../UI/Countdown";
 import NewItemsSkeleton from "../UI/NewItemsSkeleton";
+import ItemCards from "../UI/ItemCards";
 
 const NewItems = () => {
   const [newItemsData, setNewItemsData] = useState(null);
@@ -32,63 +31,7 @@ const NewItems = () => {
           </div>
           {newItemsData ? (
             <Slider {...carouselSettings}>
-              {newItemsData.map((item, index) => (
-                <div key={item.id}>
-                  <div className="nft__item mx-1">
-                    <div className="author_list_pp">
-                      <Link
-                        to={`/author/${item.authorId}`}
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        title="Creator: Monica Lucas"
-                      >
-                        <img className="lazy" src={item.authorImage} alt="" />
-                        <i className="fa fa-check"></i>
-                      </Link>
-                    </div>
-                    {item.expiryDate && (
-                      <Countdown expiryDate={item.expiryDate} />
-                    )}
-                    <div className="nft__item_wrap">
-                      <div className="nft__item_extra">
-                        <div className="nft__item_buttons">
-                          <button>Buy Now</button>
-                          <div className="nft__item_share">
-                            <h4>Share</h4>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-facebook fa-lg"></i>
-                            </a>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-twitter fa-lg"></i>
-                            </a>
-                            <a href="">
-                              <i className="fa fa-envelope fa-lg"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-
-                      <Link to={`/item-details/${item.nftId}`}>
-                        <img
-                          src={item.nftImage}
-                          className="lazy nft__item_preview"
-                          alt=""
-                        />
-                      </Link>
-                    </div>
-                    <div className="nft__item_info">
-                      <Link to="/item-details">
-                        <h4>{item.title}</h4>
-                      </Link>
-                      <div className="nft__item_price">{item.price} ETH</div>
-                      <div className="nft__item_like">
-                        <i className="fa fa-heart"></i>
-                        <span>{item.likes}</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
+              {newItemsData.map((item) => (<ItemCards key={item.id} item={item} />))}
             </Slider>
           ) : (
             <NewItemsSkeleton />
