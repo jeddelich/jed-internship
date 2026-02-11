ea1a50b8defeed7c73852763cd75b48f10f2e3d3
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-11 00:15:12 -0500

    author nfts skeleton added

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,20 +1,35 @@
 import ItemCards from "../UI/ItemCards";
+import ItemSkeleton from "../UI/ItemSkeleton";
 
 const AuthorItems = ({ author }) => {
-
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {author?.nftCollection.map((item) => (
-            <ItemCards
-              author={author}
-              key={item.id}
-              item={item}
-              wrapperClass="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 w-full"
-              wrapperStyle={{ display: "block", backgroundSize: "cover" }}
-            />
-          ))}
+          {author ? (
+            <>
+              {author.nftCollection.map((item) => (
+                <ItemCards
+                  author={author}
+                  key={item.id}
+                  item={item}
+                  wrapperClass="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 w-full"
+                  wrapperStyle={{ display: "block", backgroundSize: "cover" }}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              {new Array(8).fill("").map((item) => (
+                <ItemSkeleton
+                  key={item.id}
+                  item={item}
+                  wrapperClass="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 w-full"
+                  wrapperStyle={{ display: "block", backgroundSize: "cover" }}
+                />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </div>
